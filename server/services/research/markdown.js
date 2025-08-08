090bb547c59430f841b22a8666829d20d22bda26
Agmatica <dev@agmatica.com>
2025-08-08 14:34:58 -0500

    234p

--- server/services/research/markdown.js
+++ server/services/research/markdown.js
@@ -1,21 +1,9 @@
-const { unified } = require('unified');
-const remarkParse = require('remark-parse');
-const remarkGfm = require('remark-gfm');
-const remarkRehype = require('remark-rehype');
-const rehypeSanitize = require('rehype-sanitize');
-const rehypeSlug = require('rehype-slug');
-const rehypeStringify = require('rehype-stringify');
+const MarkdownIt = require('markdown-it');
+
+const md = new MarkdownIt({ html: false, linkify: true, typographer: true });
 
 async function renderMarkdownToHtml(markdown) {
-  const file = await unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkRehype, { allowDangerousHtml: false })
-    .use(rehypeSanitize)
-    .use(rehypeSlug)
-    .use(rehypeStringify)
-    .process(markdown || '');
-  return String(file);
+  return md.render(markdown || '');
 }
 
 module.exports = { renderMarkdownToHtml };
